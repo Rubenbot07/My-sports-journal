4a35f6316c9793fca4b7c327e6ce842c1a406b2f
rubenbot07 <rubenbot77@hotmail.com>
2025-03-28 20:30:32 -0500

    add comment functionality; implement addComment and deleteComment methods in UserContext and update Article component for comment handling

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -9,6 +9,7 @@ export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [role, setRole] = useState(null)
     const [wrongUser, setWrongUser] = useState(false)
+    const [articles, setArticles] = useState(Articles)
     const [favorites, setFavorites] = useState([])
     const navigate = useNavigate()
     const location = useLocation()
@@ -24,6 +25,7 @@ export const UserProvider = ({ children }) => {
         setUser(username)
         setRole(user.role)
         setWrongUser(false)
+        console.log(location.state)
         navigate(location.state?.from ? location.state.from.pathname : '/')
     }
 
@@ -34,21 +36,23 @@ export const UserProvider = ({ children }) => {
     }
 
     const deleteArticle = (articleId) => {
-        const articleIndex = Articles.findIndex(article => article.id === articleId)
-        Articles.splice(articleIndex, 1)
+        const articleIndex = articles.findIndex(article => article.id === articleId)
+        articles
+        .splice(articleIndex, 1)
         navigate('/')
     }
 
     const editArticle = (articleId, title, author, publishedDate, content) => {
         const articleIdParsed = parseInt(articleId)
-        const articleIndex = Articles.findIndex(article =>  article.id === articleIdParsed)
-        Articles.splice(articleIndex, 1, { id: articleIdParsed, title: title, author: author, publishedDate: publishedDate, content: content })
-        console.log(Articles)
+        const articleIndex = articles.findIndex(article =>  article.id === articleIdParsed)
+        const updatedArticle = articles.splice(articleIndex, 1, { id: articleIdParsed, title: title, author: author, publishedDate: publishedDate, content: content })
+        setArticles(updatedArticle)
         navigate(`/articles/${articleId}`)
     }
     const handleFavorites = (articleId) => {
         if(!user) {
             navigate('/login', { state: { from: location } })
+            console.log(location.state)
             return
         }
         if (favorites.some(favorite => favorite.id === articleId)) {
@@ -56,21 +60,38 @@ export const UserProvider = ({ children }) => {
             setFavorites(updatedFavorites)
             return
         }
-        const userFavorite = Articles.find(article => article.id === articleId)
+        const userFavorite = articles.find(article => article.id === articleId)
         setFavorites(prevFavorites => [...prevFavorites, userFavorite])
     }
-
+    const addComment = (articleId, comment, userName, date) => {
+        const articleIndex = articles.findIndex(article => article.id === articleId)
+        const commentObject = {
+            userName: userName,
+            id: Math.floor(Math.random() * 100000),
+            content: comment,
+            date: date
+        }
+        const updatedArticle = articles[articleIndex].comments.push(commentObject)
+        const updatedArticles = articles.splice(articleIndex, 1, { ...updatedArticle})
+        setArticles(updatedArticles)
+    }
+    const deleteComment = (articleId, commentId) => {
+        console.log(articleId, commentId)
+    }
     const auth = { 
         user, 
         setUser,
         login,
         logout,
         wrongUser,
+        articles,
         role,
         deleteArticle,
         editArticle,
         favorites,
-        handleFavorites
+        handleFavorites,
+        addComment,
+        deleteComment
     }
     return (
         <UserContext.Provider value={auth} >
--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,10 +1,19 @@
-import Articles from '../../mock-data/sports-articles.json';
 import { useParams, Link } from 'react-router-dom';
 import { useAuth } from '../../context/UserContext';
 export const Article = () => {
     const auth = useAuth()
     const { articleId } = useParams();
-    const currentArticle = Articles.find(article => article.id === parseInt(articleId));
+    const currentArticle = auth.articles[articleId - 1];
+    console.log(currentArticle.title);
+    const handleComment = (e) => {
+        e.preventDefault();
+        const comment = e.target.comment.value;
+        if (comment) {
+            auth.addComment(currentArticle.id, comment, auth.user.name, new Date().toLocaleDateString());
+            // auth.addComment(currentArticle.id, comment);
+            e.target.comment.value = '';
+        }
+    }
     return (
         <section className='text-center w-5/6 mx-auto flex flex-col gap-8'>
             <h1 className='text-2xl font-bold bg-blue-500'>{currentArticle.title}</h1>
@@ -15,6 +24,51 @@ export const Article = () => {
                     {currentArticle.author}
                 </Link>
             </h3>
+            <div>
+                <h2 className='text-xl font-bold'>Comments</h2>
+                {currentArticle.comments?.map(comment => (
+                    <div key={comment.id} className='bg-gray-200 p-2 my-2 flex gap-2 flex-col justify-between'>
+                        <h4 className='font-bold'>{comment.userName}</h4>
+                        <p>{comment.content}</p>
+                        {
+                            auth?.role === 'admin' && (
+                                <button
+                                    onClick={() => auth.deleteComment(currentArticle.id, comment.id)}
+                                    className='bg-red-500 text-white p-1 rounded-lg w-40 mx-auto'
+                                >
+                                    Delete
+                                </button>
+                            )
+                        }
+                    </div>
+                ))}
+                {
+                    auth.user ? (
+                        <form
+                            onSubmit={(e) => {handleComment(e)}
+                            }
+                            className='flex flex-col gap-2'
+                        >
+                            <input
+                                type='text'
+                                name='comment'
+                                placeholder='Add a comment...'
+                                className='border border-blue-500 p-2 rounded-lg'
+                            />
+                            <button
+                                type='submit'
+                                className='bg-blue-500 text-white p-2 rounded-lg'
+                            >
+                                Submit
+                            </button>
+                        </form>
+                    ) : (
+                        <Link to='/login'>
+                            <button className='text-red-500 border border-red-500 p-2 rounded-lg'>Log In To Leave A Comment</button>
+                        </Link>
+                    )
+                }
+            </div>
             {
                 auth.favorites.some(favorite => favorite.id === currentArticle.id) ? (
                     <button onClick={() => auth.handleFavorites(currentArticle.id)} className='bg-red-500 text-white p-2 rounded-lg'>Remove From Favorites</button>
