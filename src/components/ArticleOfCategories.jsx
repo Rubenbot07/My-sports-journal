d68af4dc23317f07905b650f4d52f24d639aa55b
rubenbot07 <rubenbot77@hotmail.com>
2025-04-09 14:10:10 -0500

    add ArticleOfCategories component and update Category and MostPopular pages for improved article display

---
+++ src/components/ArticleOfCategories.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+export const ArticleOfCategories = ({ article }) => {
+    return (
+        <Link to={`/articles/${article.id}`} >
+            <article className='relative flex flex-col md:flex-row max-w-[500px] md:max-w-none mx-auto gap-4 bg-gray-200 p-4 rounded-lg shadow-md'>
+                <div className='w-full md:w-2/4 md:min-w-70 rounded-2xl overflow-hidden h-[200px]'>
+                    <img className='w-full h-full object-cover' src={article.image} alt={article.title} />
+                </div>
+                <div className='flex flex-col md:w-2/4 text-start gap-2 pt-4 overflow-hidden'>
+                    <h2 className='text-md'>{article.publishedDate}</h2>
+                    <h3 className='font-bold text-lg text-nowrap truncate'>{article.title}</h3>
+                    <p className='hidden md:block'>{article.content.split(' ').slice(0, 20).join(' ')}...</p>
+                </div>
+            </article>
+        </Link>
+    )
+}
--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,17 +1,27 @@
 import { useParams } from "react-router-dom"
 import { useAuth } from "../../context/UserContext"
-import { ArticleLink } from "../../components/ArticleLink"
+import { ArticleOfCategories } from "../../components/ArticleOfCategories";
 export const Category = () => {
     const { category } = useParams();
     const auth = useAuth()
     const categoryTitle = category.charAt(0).toUpperCase() + category.slice(1)
     const categoryArticles = auth.articles.filter(article => article.category === categoryTitle)
     return (
-        <section>
-            <h1>{ categoryTitle }</h1>
+        <section className='grid grid-cols-1 2sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-4 p-4 w-full max-w-[1500px] mx-auto'>
+            <h1 className='font-bold  text-4xl text-red-500 col-span-1 2sm:col-span-2 md:col-span-1 lg:col-span-2'>{categoryTitle}</h1>
+            <article className="col-span-1 2sm:col-span-2 md:col-span-1 lg:col-span-2 flex flex-col md:flex-row  bg-gray-300 p-4 rounded-lg shadow-md text-start gap-4">
+                <picture className="rounded-lg overflow-hidden md:w-2/3 lg:w-1/2 max-h-96">
+                    <img className="w-full h-full object-cover object-top" src={categoryArticles[0].image} alt={categoryArticles[0].title} />
+                </picture>
+                <div className="flex flex-col gap-2 lg:gap-4 p-2 md:w-1/3 lg:w-1/2 overflow-hidden">
+                    <h2 className='text-md'>{categoryArticles[0].publishedDate}</h2>
+                    <h3 className='font-bold text-lg 2sm:text-2xl md:text-3xl lg:text-5xl'>{categoryArticles[0].title}</h3>
+                    <p className="md:text-lg lg:text-xl">{categoryArticles[0].content.split(' ').slice(0, 30).join(' ')}...</p>
+                </div>
+            </article>
             {
-                categoryArticles.map(article => (
-                    <ArticleLink key={article.id} article={article} />
+                categoryArticles.splice(1).map(article => (
+                    <ArticleOfCategories key={article.id} article={article} />
                 ))
             }
         </section>
